30a61e35752592a182284fa6df9d2209eefa28ee
lassev05 <vestergaardlasse2@gmail.com>
2022-10-14 20:28:25 +0200

    Build System

--- src/includer.js
+++ src/includer.js
@@ -1,29 +1,28 @@
-const INC = document.getElementById("inc");
+const INC_LIST = document.querySelectorAll("#inc");
 
-const ATTR = INC.getAttribute("file");
 const END_PREFIX = ".html";
 
-ATTR.map((a) => {
-  console.log(a);
-});
+INC_LIST.forEach((I) => {
+  let ATTR = I.getAttribute("file");
 
-// Fetcher
-function getFile() {
-  if (!ATTR.includes(END_PREFIX)) {
-    throw new Error(`The path to ${ATTR}, must end with ${END_PREFIX}`);
+  function getFile() {
+    if (!ATTR.includes(END_PREFIX)) {
+      throw new Error(`The path to ${ATTR}, must end with ${END_PREFIX}`);
+    }
+
+    // Get the HTML file
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("GET", `${window.location.href}${ATTR}`);
+
+    xhr.onload = function () {
+      if (this.status == 200) {
+        console.log(this.responseText);
+      }
+    };
+
+    xhr.send();
   }
 
-  // Get the HTML file
-  let xhr = new XMLHttpRequest();
-
-  xhr.open("GET", `${window.location.href}${ATTR}`);
-
-  xhr.onload = function () {
-    if (this.status == 200) {
-    }
-  };
-
-  xhr.send();
-}
-
-// getFile();
+  getFile();
+});
