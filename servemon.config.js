0a20c4befefb294ed593e393c3fe5256d84da6ac
lassev05 <vestergaardlasse2@gmail.com>
2022-10-16 15:06:09 +0200

    Update servemon config

--- servemon.config.js
+++ servemon.config.js
@@ -1,14 +1,7 @@
 module.exports = {
-    projectName: "includer.js",
-    port: 3000, // Port to use
-    directory: "src", // Directory to use
-    pkgManager: "servemon", // Package manager you want to use
-    watch: true,  // Watch for changes
-    open: false, // Open the browser
-    fileExplore: false, // Enable file explorer
-    logger: false, // Enable logger
-
-    
-}
-
-    
+  projectName: "includer.js",
+  port: 3000,
+  directory: "src",
+  pkgManager: "servemon",
+  watch: true,
+};
